40e1e7b2e16e092bd9f2750de0a070eee8d3f431
Luiz Otavio <luiz.otav.2009@gmail.com>
2025-07-31 07:58:44 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("link");
+  const button = document.getElementById("generate-button");
+  const qrContainer = document.getElementById("qrcode");
+
+  button.addEventListener("click", () => {
+    const url = input.value.trim();
+    if (!url) {
+      alert("Please enter a link.");
+      return;
+    }
+
+    qrContainer.innerHTML = ""; // <- Aqui só funciona se qrContainer existir!
+
+    new QRCode(qrContainer, {
+      text: url,
+      width: 250,
+      height: 250,
+      colorDark: "#000000",
+      colorLight: "#D9D9D9",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+  });
+});
